d3b9473be6ac285b4aa192eef3699c1daf2f9ff7
Suda Nimh <rslepejian@gmail.com>
2021-01-19 16:22:37 -0800

    filled out readme, added images and urls for opensource licenses in script

--- README.md
+++ README.md
@@ -1,27 +1,40 @@
-# Portfolio
+# Readme Generator
 
 ## Description
-
-## Table of Contents
-- [Installation](#Installation)
-- [Usage](#Usage)
-- [License](#License)
-- [Contributing](#Contributing)
-- [Tests](#Tests)
-- [Questions](#Questions)
-
-## Installation
-
-## Usage
+This is an application that runs in the command line using node to generate a readme file. The user is asked a series of questions and a readme is generated based on their responses.
 
 ## License
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-## Contributing
+## Built With
 
-## Tests
+* [Javascript](https://www.javascript.com/)
+* [Node](https://nodejs.org/en/)
+* [Inquirer](https://www.npmjs.com/package/inquirer)
+* [Git]
+* [Github](https://github.com/)
 
-## Questions
+## Demo of Working App
 
-- Github: 
-- Email:
+Video link: (https://drive.google.com/file/d/1zjRYV3IB2E0pCGtcWwso6ydBX8V6YtQN/view?usp=sharing)
+
+## Code Snippet
+This code snippet shows one of the inquirer prompt that is used to get data from the user to populate the readme.
+
+```javascript
+        {
+            type: 'input',
+            message: 'What would you like the description of your project to be?',
+            name: 'description'
+        },
+```
+
+## Authors
+
+* **Raffi Lepejian** 
+
+## Contact Information
+
+- [Link to Portfolio Site](https://rslepejian.github.io/portfolio/)
+- [Link to Github](https://github.com/rslepejian)
+- [Link to LinkedIn](https://linkedin.com/in/raffi-lepejian-071876153)
--- script.js
+++ script.js
@@ -57,9 +57,46 @@ async function generateReadme() {
         }
         ]);
 
+        if (project.license == 'MIT'){
+            var licenseIMG = 'https://img.shields.io/badge/License-MIT-yellow.svg';
+            var licenseURL= 'https://opensource.org/licenses/MIT';
+        }
+        else if (project.license == 'GNU GPLv3'){
+            var licenseIMG = 'https://www.gnu.org/graphics/gplv3-127x51.png';
+            var licenseURL= 'https://opensource.org/licenses/GPL-3.0';
+        }
+        else if (project.license == 'GNU AGPLv3'){
+            var licenseIMG = 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Shopware-Licence-AGPL3.png';
+            var licenseURL= 'https://opensource.org/licenses/AGPL-3.0';
+        }
+        else if (project.license == 'GNU LGPLv3'){
+            var licenseIMG = 'https://lh3.googleusercontent.com/proxy/J2zfsRkJFMeKR0inzyNbvVeOjzKwuY1zjR290gnp9036jeR5tggPakyQYLo-oOySrBq3nloS1ygi44jn5-MQmWX4Iz4ucrLNHnyq';
+            var licenseURL= 'https://opensource.org/licenses/LGPL-3.0';
+        }
+        else if (project.license == 'Mozilla Public License 2.0'){
+            var licenseIMG = 'https://lh3.googleusercontent.com/proxy/mU6BzURBPA8fNX_H8gM3rr7hfWEOE4ZWg4kanGRo36FurfkNlJT0akqofKb5GKoVdMHceFtIs0waxXtn6MRSDeMwdhL-FbEXcuiOHGD_cPq9DJiArU5I-Y48kFUzvwP3gjIg3V2oAhA0fv4vOEw';
+            var licenseURL= 'https://opensource.org/licenses/MPL-2.0';
+        }
+        else if (project.license == 'Apache License 2.0'){
+            var licenseIMG = 'https://www.apache.org/foundation/press/kit/Badge_2_APACHE_20th_Anniversary.png';
+            var licenseURL= 'https://opensource.org/licenses/Apache-2.0';
+        }
+        else if (project.license == 'Boost Software License 1.0'){
+            var licenseIMG = 'https://www.google-melange.com/archive/gsoc/2015/orgs/boostcpp/logo-200.png';
+            var licenseURL= 'https://opensource.org/licenses/BSL-1.0';
+        }
+        else {
+            // the unlicense
+            project.license = 'The Unlicense';
+            var licenseIMG = 'https://unlicense.org/pd-icon.png';
+            var licenseURL= 'https://opensource.org/licenses/unlicense';
+        }
+
 
         const readme = 
         `# ${project.title}
+
+[![License: ${project.license}](${licenseIMG})](${licenseURL})
 
 ## Description
         
@@ -82,7 +119,7 @@ ${project.installation}
 ${project.usage}
         
 ## License
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+This project is licensed under the ${project.license} license.
         
 ## Contributing
         
